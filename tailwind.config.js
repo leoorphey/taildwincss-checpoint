57a4d44e89a11289a572d09da21978f2c0698673
leoorphey <leodurent18.loeb@gmail.com>
2024-08-15 20:10:11 +0000

    checkpoint taildwin

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        GregorianFLF : ['GregorianFLF ', 'sans-serif'],
+        BaskervvilleSC: ['BaskervvilleSC', 'sana-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
+
